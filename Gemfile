34294b2a1fb27de9642de4c08a204c3263064917
Ihar <iharprokop@gmail.com>
2018-04-18 14:11:43 +0300

    for kos

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem "sinatra"
 gem "sinatra-contrib"
+gem 'sinatra-twitter-bootstrap', :require => 'sinatra/twitter-bootstrap'
--- main.rb
+++ main.rb
@@ -3,8 +3,10 @@ require "sinatra"
 require "sinatra/config_file"
 require "./lib/game"
 require "./lib/player"
-# require 'securerandom'
-# set :session_secret, SecureRandom.hex(64)
+require 'securerandom'
+require 'sinatra/twitter-bootstrap'
+require 'haml'
+
 use Rack::Session::Pool
 config_file './config/items.yml'
 
@@ -12,23 +14,24 @@ get '/' do
   @items = settings.items
 
   player1_pick = @items.sample
-  digest = OpenSSL::Digest.new('sha1')
+  digest = OpenSSL::Digest.new('sha256')
   key = SecureRandom.hex(64)
   session[:player1_pick] = player1_pick
   session[:secret] = key
 
-  instance = OpenSSL::HMAC.hexdigest(digest, key, player1_pick)
+  instance = OpenSSL::HMAC.hexdigest('SHA256', key, "spock")
   @hmac_hash = instance.to_s
 
   erb :index
 end
 
+
 get '/game' do
   player1_pick = session[:player1_pick]
   key = session[:secret]
 
-  digest = OpenSSL::Digest.new('sha1')
-  instance = OpenSSL::HMAC.hexdigest(digest, key, player1_pick)
+  digest = OpenSSL::Digest.new('sha256')
+  instance = OpenSSL::HMAC.hexdigest('SHA256', key, "spock")
   @hmac_hash = instance.to_s
 
   session[:player1_pick] = nil
